221ac1f8e63c69eff9659e6f9315cc9d9cb7bc60
ProfI2S101 <ProfI2S101@lab.aca>
2025-01-28 11:30:50 -0500

    Formulario gsntionado

--- src/components/FormularioLoginSeccion.tsx
+++ src/components/FormularioLoginSeccion.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
+
 const FormularioLoginSeccion = () => {
-    
+    const [usuario, setUsuario] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const handleUsuarioChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setUsuario(e.currentTarget.value)
+    }
+
+    const handlePasswordChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
     return <form>
         <div>
             <label className="form-label">Usuario:</label>
-            <input className="form-control" type="text" />
+            <input className="form-control" 
+                    type="text"
+                    value={ usuario }
+                    onChange={ handleUsuarioChange } />
         </div>
         <div>
             <label className="form-label">Password:</label>
-            <input className="form-control" type="password" />
+            <input className="form-control" 
+                    type="password"
+                    value={ password }
+                    onChange={ handlePasswordChange } />
         </div>
         <button id="margen-superior-boton" 
             className="btn btn-success" 
