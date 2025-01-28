195777c6435c422679777f65a798fddba23687e4
ProfI2S101 <ProfI2S101@lab.aca>
2025-01-28 12:29:52 -0500

    Agregando modal

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,12 @@ import { Link, NavLink, useNavigate } from "react-router-dom"
 import FormularioLoginSeccion from "../components/FormularioLoginSeccion"
 import TitulosSeccion from "../components/TitulosSeccion"
 import "./LoginPage.css"
+import { useState } from "react"
 
 const LoginPage = () => {
     const navigate = useNavigate()
+
+    const [showModal, setShowModal] = useState<boolean>(false)
 
     const loginHandler = (usuario : string, password:string) => {
         if (usuario == "PW" && password == "123") {
@@ -12,6 +15,7 @@ const LoginPage = () => {
             navigate("/main")
         }else {
             // Login incorrecto
+            setShowModal(true)
         }
     }
 
@@ -20,7 +24,29 @@ const LoginPage = () => {
         <div className="col">
             <TitulosSeccion />
             <FormularioLoginSeccion onLogin={ loginHandler }/>
-            <Link to={"/main"} className="btn btn-danger">Ir a MainPage</Link>
+            <div className={ showModal == true ? "modal fade show d-block" : "modal fade" } >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Error</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Login y/o password incorrecto.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" 
+                                className="btn btn-secondary"
+                                onClick={ () =>  {
+                                    setShowModal(false)
+                                } }>
+                                Cerrar
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="col"></div>
     </div>
