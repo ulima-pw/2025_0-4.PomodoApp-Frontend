613f882877e663c4e31ed91c63e15ab6e0719c72
ProfI2S101 <ProfI2S101@lab.aca>
2025-01-29 12:33:53 -0500

    ListadoProyectos inciado

--- src/components/ListadoProyectos.tsx
+++ src/components/ListadoProyectos.tsx
@@ -1,6 +1,57 @@
 const ListadoProyectos = () => {
     return <>
-        <h2>Proyectos</h2>
+        <h2>
+            Proyectos
+            <button type="button"
+                    className="btn btn-primary">
+                +
+            </button>
+        </h2>
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Nro Pom</th>
+                    <th>Status</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Proy 1</td>
+                    <td>1</td>
+                    <td>Iniciado</td>
+                    <td>
+                        <button type="button"
+                                className="btn btn-primary me-2">
+                            I
+                        </button>
+                        <button type="button"
+                                className="btn btn-primary">
+                            E
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Proy 1</td>
+                    <td>1</td>
+                    <td>Iniciado</td>
+                    <td>
+                        <button type="button"
+                                className="btn btn-primary me-2">
+                            I
+                        </button>
+                        <button type="button"
+                                className="btn btn-primary">
+                            E
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </>
 }
 
