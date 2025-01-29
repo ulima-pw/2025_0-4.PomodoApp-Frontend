d58b252faa02fcb2ad59608b4f694233cf1d8118
ProfI2S101 <ProfI2S101@lab.aca>
2025-01-29 12:13:32 -0500

    Component MenuOpciones creado

---
+++ src/components/MenuOpciones.tsx
@@ -0,0 +1,18 @@
+const MenuOpciones = () => {
+    return <ul className="list-group">
+        <li className="list-group-item active">
+            Mis Proyectos
+        </li>
+        <li className="list-group-item ">
+            Histórico
+        </li>
+        <li className="list-group-item ">
+            Configuración
+        </li>
+        <li className="list-group-item ">
+            Cerrar Sesión
+        </li>
+    </ul>
+}
+
+export default MenuOpciones
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,17 @@ import { useNavigate } from "react-router-dom"
 const MainPage = () => {
     const navigate = useNavigate()
     return <div>
-        <h1>MainPage</h1>
+        <h1>Pomodoro UL</h1>
+        <div className="row">
+            <div className="col-md-4">
+                <MenuOpciones />
+            </div>
+            <div className="col-md-8">
+                <ListaProyectos />
+            </div>
+        </div>
+        <Footer />
+
         <button type="button"
         className="btn btn-danger"
                 onClick={ () => {
