60c8c06d889232922b6c982dbea3162b1612e6e3
ProfI2S101 <ProfI2S101@lab.aca>
2025-01-29 11:56:22 -0500

    uso de sessionStorage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { Link, NavLink, useNavigate } from "react-router-dom"
 import FormularioLoginSeccion from "../components/FormularioLoginSeccion"
 import TitulosSeccion from "../components/TitulosSeccion"
 import "./LoginPage.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ModalFormularioLogin from "../components/ModalFormularioLogin"
 
 const LoginPage = () => {
@@ -10,9 +10,24 @@ const LoginPage = () => {
 
     const [showModal, setShowModal] = useState<boolean>(false)
 
+    // Se va a ejecutar al terminar el renderizado del componente
+    useEffect(() => {
+        const usuario = sessionStorage.getItem("usuario")
+        if (usuario != null) {
+            navigate("/main")
+        }
+    })
+
     const loginHandler = (usuario : string, password:string) => {
         if (usuario == "PW" && password == "123") {
             // Login correcto
+            const user = {
+                usuario : usuario,
+                password : password
+            }
+            const userJSON = JSON.stringify(user)
+            console.log(userJSON)
+            sessionStorage.setItem("usuario", userJSON)
             navigate("/main")
         }else {
             // Login incorrecto
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 const MainPage = () => {
-    return <div>MainPage</div>
+    const navigate = useNavigate()
+    return <div>
+        <h1>MainPage</h1>
+        <button type="button"
+        className="btn btn-danger"
+                onClick={ () => {
+                    sessionStorage.removeItem("usuario")
+                    navigate("/")
+                } } >
+            Logout
+        </button>
+    </div>
 }
 
 export default MainPage
