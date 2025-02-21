e02234d7e2516ff0581ee237e6ce43b2242ee2d0
ProfO2204 <ProfO2204@lab.aca>
2025-02-21 12:48:23 -0500

    fix de url de imagen

--- src/components/TitulosSeccion.tsx
+++ src/components/TitulosSeccion.tsx
@@ -1,10 +1,12 @@
+const URL_BACKEND = import.meta.env.VITE_URL_BACKEND || "http://localhost:5000"
+
 const TitulosSeccion = () => {
     return <>
         <h1>PomodoroApp</h1>
         <h2>Login</h2>
         <img
             style={{ width: "100%" }} 
-            src="http://localhost:5000/logo.png" />
+            src= {URL_BACKEND + "/logo.png"} />
     </>
 }
 
